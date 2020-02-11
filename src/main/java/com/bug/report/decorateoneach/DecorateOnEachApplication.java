9d7eb933e3884d87f4422fc2007d054b904cfcc1
creator.koo <creator.koo@icloud.com>
2020-02-11 14:40:25 +0900

    init

---
+++ src/main/java/com/bug/report/decorateoneach/DecorateOnEachApplication.java
@@ -0,0 +1,13 @@
+package com.bug.report.decorateoneach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorateOnEachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecorateOnEachApplication.class, args);
+    }
+
+}
